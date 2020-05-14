4c2583103ac1e5fc56dcf532c443de1eb2bf5ffd
moyersjm <moyersjm@rose-hulman.edu>
2020-05-14 16:59:57 -0400

    Okay don't panic

--- DungeonDatabase/src/dungeondatabase/services/Frag_Items.java
+++ DungeonDatabase/src/dungeondatabase/services/Frag_Items.java
@@ -2,9 +2,12 @@ package dungeondatabase.services;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Color;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -56,6 +59,18 @@ public class Frag_Items {
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1.setBounds(10, 40, 184, 25);
 		frame.getContentPane().add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Add Item");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNewButton.setBounds(200, 39, 200, 25);
+		btnNewButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) { //Open create character menu
+				Frag_Create_Item window = new Frag_Create_Item();
+				window.frame.setVisible(true);
+			}
+		});
+		frame.getContentPane().add(btnNewButton);
 		
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
